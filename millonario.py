fe0046f303d813e8f3833fa1f1a21a3f98f8e29a
valentinperez98 <isudinel47@gmail.com>
2022-08-04 16:29:36 -0400

    quien quiere ser millonario

---
+++ millonario.py
@@ -0,0 +1,34 @@
+vida=3
+
+print ('Quien quiere ser Millonario??')
+print ('Tiene 3 vidas')
+print ('1: ¿Cuantos lados tiene un cubo?')
+
+print ('1=8')
+print ('2=6')
+print ('3=10')
+print ('4=4')
+op=input
+opB=2 
+
+if op == opB:
+    print ('Correcto! Continuar')
+else:
+    print ('Incorrecto, Pierdes 1 vida, quedan 2')
+    vida-=1
+
+print ('2: ¿Quien es el mejor amigo del hombre?')
+
+print ('1=Perro')
+print ('2=Gato')
+print ('3=Loros')
+print ('4=Hamsters')
+
+op1=input
+op2=1
+if op1 == op2:
+    print ('Correcto! Continuar')
+else:
+    print ('Incorrecto, Pierdes 1 vida, quedan 2')
+    vida-=1
+
